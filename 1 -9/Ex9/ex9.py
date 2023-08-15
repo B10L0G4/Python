2ca2945319f66428b29e7ff77d0e3d49b97d7b2f
B10l0g4 <vandyyh@gmail.com>
2023-08-15 16:45:54 -0300

    refactor ex9 e inclusão de verificação para celcius

--- 1 -9/Ex9/ex9.py
+++ 1 -9/Ex9/ex9.py
@@ -1,10 +1,17 @@
 # Faça um Programa que peça a temperatura em graus Fahrenheit,
-# transforme e mostre a temperatura em graus Celsius.
+# transforme e mostre a temperatura em graus Celsius e o inverso .
 # C = 5 \* ((F-32) / 9).
 
-fahrenheitGraus = float(input('Qual a temperatura em graus fahrenheit hj? '))
 
-celsius = (fahrenheitGraus - 32) / 1.8
+tipoF= input('Você deseja converter qual tipo de temperatura ? \n Para Fahreheit digite f: \n Para Celsius digite c:\n')
 
-print(f'A temperatura em graus Fahrenheit hoje é {fahrenheitGraus}F° '
-      f'e sua temperatura equivalente em graus Celsius é {celsius:.1f}C°')
+if tipoF == 'f' :
+      graus = float(input('Qual a temperatura \n'))
+      celsius = (graus - 32) / 1.8
+      print(f'A temperatura em graus Fahrenheit hoje é {graus}F°'
+            f' e sua temperatura equivalente em graus Celsius é {celsius:.1f}C°')
+else:
+      graus = float(input('Qual a temperatura \n'))
+      fahrenheit = (graus * 1.8 ) + 32
+      print(f'A temperatura em graus Celsius é  hoje é {graus}F° '
+            f'e sua temperatura equivalente em graus Fahrenheit é {fahrenheit:.1f}f°')
--- 1 -9/README.md
+++ 1 -9/README.md
@@ -15,5 +15,5 @@
 > 8 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
 
 > 9 - Faça um Programa que peça a temperatura em graus Fahrenheit,
-> transforme e mostre a temperatura em graus Celsius.
+> transforme e mostre a temperatura em graus Celsius e o inverso .
 > C = 5 \* ((F-32) / 9).
