65e825ba3419c7f727ccf80f66553e4bfe664c83
B10l0g4 <vandyyh@gmail.com>
2023-08-16 19:19:03 -0300

    exemplos de aula

---
+++ 1 -9/Ex9/teste.py
@@ -0,0 +1,138 @@
+# def func(a,b):
+#     return b**a
+#
+# print(func(b=2, 2))
+
+# z =0
+# y =10
+#
+# x = y < z and z > y or y < z and z < y
+#
+# print(x)
+# my_list = [x *x for x in range(5)]
+#
+# def func(lst):
+#     del lst[lst[2]]
+#     return lst
+#
+# print(func(my_list))
+
+# a = 1
+# b =0
+# a = a ^ b
+# b = a ^ b
+# a = a ^ b
+#
+# print(a, b)
+
+# def fun(x):
+#     if x % 2 == 0:
+#         return 1
+#     else:
+#         return 2
+# print(fun(fun(2)))
+
+# nums = [1,2,3]
+# vals = nums
+# del vals[:]
+#
+# print(nums)
+# print(vals)
+
+# x = int(input('Digite um número inteiro: '))
+# y = int(input('Digite outro número inteiro: '))
+# x = x % y
+# x = x % y
+# y = y % x
+# print(y)
+#
+# x = float(input('Digite um número: '))
+# y = float(input('Digite outro número: '))
+# print(y ** (1/x))
+
+# my_tuple = (1,2,3)
+# my_tuple[1] = my_tuple[1] + my_tuple[2]
+#
+# print(my_tuple)
+
+
+# dct = {'one':'two', 'three':'one', 'two':'three'}
+# v = dct['one']
+#
+# for k in range(len(dct)):
+#     v = dct[v]
+# print(v)
+
+# lst = [i for i in range(-1,-2)]
+# print(len(lst))
+
+# def fun(x,y):
+#     if x == y:
+#         return x
+#     else:
+#         return fun(x, y-1)
+#
+# print(fun(0,3))
+
+# i = 0
+# while i < i + 2:
+#     i += 1
+#     print('*')
+# else:
+#     print('*')
+
+# tup = (1,2,4,8)
+# tup = tup[-2:-1]
+# tup = tup[-1]
+# print(tup)
+
+# dd= {"1":"0","0":"1"}
+# for x in dd.vals():
+#     print(x, end="")
+
+# dct = {}
+# dct['1'] = (1,2)
+# dct['2'] = (2,1)
+#
+# for x in dct.keys():
+#     print(dct[x][1], end="")
+#
+#
+# def fun(inp=2, out=3):
+#     return inp * out
+#
+# print(fun(out=2))
+
+# lst = [[x for x in range(3)] for y in range(3)]
+# for r in range(3):
+#     for c in range(3):
+#         if lst[r][c] % 2 != 0:
+#             print("#")
+
+# try:
+#     value = int(input("Digite um número: "))
+#     print(int(value)/len(value))
+# except (ValueError, ZeroDivisionError, TypeError):
+#     print("Entrada ruim")
+# except ZeroDivisionError:
+#     print("Entrada muito ruim")
+# except TypeError:
+#     print("Entrada muito muito ruim")
+# except:
+#     print("Boaaa!")
+
+print(BoaaaHola Mundo!)
+
+# try:
+#     print(5/0)
+#     break
+# except:
+#     print("Erro")
+# except (ValueError, ZeroDivisionError):
+#     print("Erro 2")
+
+# foo = (1,2,3)
+# foo.index(0)
+#
+# print(foo)
+
